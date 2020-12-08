484ce8f858fdcb1be97f681859846428bdacb647
Jamal Fedaoui <jamal.fedaoui00@gmail.com>
2020-12-08 22:49:29 +0200

    user-management

---
+++ deleteVideo.php
@@ -0,0 +1,14 @@
+<?php
+//$db = new PDO("mysql:dbname=hw6", "root", "");
+$id = $_GET["id"];
+if (!isset($id))
+	die("Missing querystring parameter!");
+else{
+
+$conn = new PDO("mysql:host=localhost;dbname=278project","root","");
+$sql1 =$conn->query("DELETE FROM Report WHERE video = '$id'");
+$sql2 =$conn->query("DELETE FROM Video WHERE id = '$id'");
+print "done";
+	header("Location: user_management.php");
+}
+	?>
